a3fddb29a3ae91fa4be80c7d76e4244453a84242
Андрей Романов <romanov_andrey_vl@protonmail.com>
2019-12-31 10:46:04 +0300

    docker fix

--- backend/docker/app/entrypoint.sh
+++ backend/docker/app/entrypoint.sh
@@ -3,6 +3,20 @@
 set -o errexit
 set -o nounset
 
+mongo_ready(){
+  dockerize -wait tcp://std_mongo:27017 -timeout 10s
+}
+
+neo4j_ready(){
+  dockerize -wait tcp://std_neo4j:7474 -timeout 10s
+}
+
+until mongo_ready && neo4j_ready; do
+  >&2 printf '\nDatabases unavailable - sleeping'
+  >&2 printf '\n'
+  >&2 printf '\n'
+done
+
 cmd="$*"
 
 if [ "$1" = 'launch' ]; then
