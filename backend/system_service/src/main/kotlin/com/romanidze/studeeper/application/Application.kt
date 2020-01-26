bddcde558ff6192ffbfab81d941a9a16d181a9f6
Андрей Романов <romanov_andrey_vl@protonmail.com>
2020-01-26 13:07:17 +0300

    exception handling

--- backend/system_service/src/main/kotlin/com/romanidze/studeeper/application/Application.kt
+++ backend/system_service/src/main/kotlin/com/romanidze/studeeper/application/Application.kt
@@ -20,7 +20,8 @@ import org.springframework.context.annotation.ComponentScan
             "com.romanidze.studeeper.modules.graphods",
             "com.romanidze.studeeper.modules.employer",
             "com.romanidze.studeeper.modules.files",
-            "com.romanidze.studeeper.modules.worker"
+            "com.romanidze.studeeper.modules.worker",
+            "com.romanidze.studeeper.modules.exceptions.handlers"
         ]
 )
 class Application
