064497f451f4d4554a1236494b84eaa11ffd932d
Андрей Романов <romanov_andrey_vl@protonmail.com>
2020-01-30 16:40:53 +0300

    main page finished

--- frontend/system_front/src/components/MainPage.vue
+++ frontend/system_front/src/components/MainPage.vue
@@ -1,38 +1,55 @@
 <template>
-  <section class="hero is-success is-medium">
-   <div class="hero-head">
-     <nav class="navbar">
-       <div class="container">
-         <div class="navbar-brand">
-          <a class="navbar-item">
-            <img src="../assets/logo.png" alt="Logo">
-          </a>
-        </div>
-         <div id="navbarMenuHeroA" class="navbar-menu">
-           <div class="navbar-end">
-             <a class="navbar-item is-active">
-               Главная страница
-             </a>
-             <a class="navbar-item">
-               Личный кабинет
-             </a>
+  <div>
+    <section class="hero is-success is-fullheight">
+      <div class="hero-head">
+       <nav class="navbar">
+         <div class="container">
+           <div class="navbar-brand">
+            <a class="navbar-item">
+              <img src="../assets/logo.png" alt="Logo">
+            </a>
+          </div>
+           <div id="navbarMenuHeroA" class="navbar-menu">
+             <div class="navbar-end">
+               <a class="navbar-item is-active">
+                 Главная страница
+               </a>
+               <a class="navbar-item">
+                 Личный кабинет
+               </a>
+             </div>
            </div>
          </div>
-       </div>
-     </nav>
-   </div>
-
-   <div class="hero-body">
-     <div class="container has-text-centered">
-       <h1 class="title">
-         Studeeper
-       </h1>
-       <h2 class="subtitle">
-         Сервис для поиска места в жизни
-       </h2>
+       </nav>
      </div>
-   </div>
-  </section>
+     <div class="hero-body">
+       <div class="container has-text-centered">
+         <h1 class="title">
+           Studeeper
+         </h1>
+         <h2 class="subtitle">
+           Сервис для поиска места в жизни
+         </h2>
+       </div>
+     </div>
+    </section>
+    <section class = "hero is-info is-medium">
+      <div class="hero-body">
+       <div class="container has-text-centered">
+         <h1 class="title">
+           Начать использовать сервис
+           <br/>
+           <br/>
+         </h1>
+         <h2 class="subtitle has-text-centered">
+          <div class="buttons">
+            <b-button type="is-danger" expanded>Войти</b-button>
+          </div>
+         </h2>
+       </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
