092dd03f48e90664037095ad662217c5ccf9ed4c
Андрей Романов <romanov_andrey_vl@protonmail.com>
2020-01-30 16:07:35 +0300

    main page

--- frontend/system_front/src/components/MainPage.vue
+++ frontend/system_front/src/components/MainPage.vue
@@ -1,11 +1,42 @@
 <template>
-  <div>
-    <h1>TEST</h1>
-  </div>
+  <section class="hero is-success is-medium">
+   <div class="hero-head">
+     <nav class="navbar">
+       <div class="container">
+         <div class="navbar-brand">
+          <a class="navbar-item">
+            <img src="../assets/logo.png" alt="Logo">
+          </a>
+        </div>
+         <div id="navbarMenuHeroA" class="navbar-menu">
+           <div class="navbar-end">
+             <a class="navbar-item is-active">
+               Главная страница
+             </a>
+             <a class="navbar-item">
+               Личный кабинет
+             </a>
+           </div>
+         </div>
+       </div>
+     </nav>
+   </div>
+
+   <div class="hero-body">
+     <div class="container has-text-centered">
+       <h1 class="title">
+         Studeeper
+       </h1>
+       <h2 class="subtitle">
+         Сервис для поиска места в жизни
+       </h2>
+     </div>
+   </div>
+  </section>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class MainPage extends Vue {}
