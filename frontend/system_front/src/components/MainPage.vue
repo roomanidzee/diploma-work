2e16a84fee74d514cac0b41f42d5626bb1533e4d
Андрей Романов <romanov_andrey_vl@protonmail.com>
2020-01-30 15:15:59 +0300

    main page start

---
+++ frontend/system_front/src/components/MainPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>TEST</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class MainPage extends Vue {}
+</script>
