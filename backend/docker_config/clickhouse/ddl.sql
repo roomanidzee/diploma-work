fd0c4ba4eda2415d7de5eb9bee57d1ab879a911f
Андрей Романов <romanov_andrey_vl@protonmail.com>
2020-03-15 21:07:17 +0300

    clickhouse setup

---
+++ backend/docker_config/clickhouse/ddl.sql
@@ -0,0 +1,83 @@
+
+CREATE TABLE IF NOT EXISTS mongo_profiles (
+    _id String,
+    user_id String,
+    surname String,
+    name String,
+    patronymic String
+) Engine = Dictionary(mongo_profiles_dictionary);
+
+CREATE TABLE IF NOT EXISTS mongo_facilities(
+    _id String,
+    title String,
+    speciality String
+) Engine = Dictionary(mongo_facilities_dictionary);
+
+CREATE TABLE IF NOT EXISTS mongo_graphods(
+    _id String,
+    facility_id String,
+    profile_id String,
+    groupmates Array(String)
+) Engine = Dictionary(mongo_graphods_dictionary);
+
+CREATE TABLE IF NOT EXISTS student_files(
+    timestamp UInt64,
+    username String,
+    raw_password String,
+    surname String,
+    name String,
+    patronymic String,
+    facility_title String,
+    facility_graduation String,
+    facility_speciality String,
+    facility_group String
+) Engine = Kafka SETTINGS
+            kafka_broker_list = 'std_kafka:9092',
+            kafka_topic_list = 'student_file_source',
+            kafka_group_name = 'student_files',
+            kafka_format = 'JSONEachRow',
+            kafka_num_consumers = 1;
+
+CREATE TABLE IF NOT EXISTS student_files_stats(
+    timestamp UInt64,
+    record_time DateTime DEFAULT NOW(),
+    username String,
+    raw_password String,
+    surname String,
+    name String,
+    patronymic String,
+    facility_title String,
+    facility_graduation String,
+    facility_speciality String,
+    facility_group String
+) ENGINE = MergeTree()
+ORDER BY timestamp;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS student_files_consumer TO student_files_stats
+  AS SELECT * FROM student_files;
+
+CREATE TABLE IF NOT EXISTS requests_source(
+    timestamp UInt64,
+    vacancy_id String,
+    employer_id String,
+    worker_id String,
+    request_status String
+) Engine = Kafka SETTINGS
+            kafka_broker_list = 'std_kafka:9092',
+            kafka_topic_list = 'request_source',
+            kafka_group_name = 'persons_requests',
+            kafka_format = 'JSONEachRow',
+            kafka_num_consumers = 1;
+
+CREATE TABLE IF NOT EXISTS requests_stats(
+    timestamp UInt64,
+    record_time DateTime DEFAULT NOW(),
+    vacancy_id String,
+    employer_id String,
+    worker_id String,
+    request_status String
+) ENGINE = MergeTree()
+ORDER BY timestamp;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS requests_consumer TO requests_stats
+  AS SELECT * FROM requests_source;
