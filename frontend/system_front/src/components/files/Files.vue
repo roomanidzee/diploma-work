696c29ba7ef498da2067e889da0775a03fdc54df
Андрей Романов <romanov_andrey_vl@protonmail.com>
2020-05-30 12:30:37 +0300

    refactoring

--- frontend/system_front/src/components/files/Files.vue
+++ frontend/system_front/src/components/files/Files.vue
@@ -1,5 +1,18 @@
 <template>
-    <Card/>
+    <div>
+      <b-field class="file">
+        <b-upload v-model="file">
+            <a class="button is-primary">
+                <b-icon icon="upload"></b-icon>
+                <span>Загрузить файл</span>
+            </a>
+        </b-upload>
+        <span class="file-name" v-if="file">
+            {{ file.name }}
+        </span>
+      </b-field>
+      <Card/>
+    </div>
 </template>
 
 <script lang="ts">
--- frontend/system_front/src/components/requests/RequestInfo.vue
+++ frontend/system_front/src/components/requests/RequestInfo.vue
@@ -3,14 +3,6 @@
     <div class="hero-body">
       <div class="container">
         <div class="card">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img
-                src="https://bulma.io/images/placeholders/1280x960.png"
-                alt="Изображение заявителя"
-              />
-            </figure>
-          </div>
 
           <div class="card-content">
             <div class="content">Здесь будет информация о заявке</div>
